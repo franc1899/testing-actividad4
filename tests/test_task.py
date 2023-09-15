2b1656c1faeab4b500a52029656139ef19d22602
franc1899 <francisco.ordenesv@uc.cl>
2023-09-15 00:19:20 -0300

    finished

--- tests/test_task.py
+++ tests/test_task.py
@@ -5,16 +5,28 @@ import unittest
 
 class TestTodoList(unittest.TestCase):
 
-    def test_A(self):
-        t1 = Task(1,"t1",False)
-        
-        self.assertEqual(t1.__str__(),"Task 1: t1 (Priority: False, Not Done)")
-    
+    def test_init(self):
+        t1 = Task(2,"t2", 1, False)
+        self.assertEqual(t1.task_id,2)
+        self.assertEqual(t1.description,"t2")
+        self.assertEqual(t1.priority,1)
+        self.assertEqual(t1.completed,False)
 
-    def test_C(self):
+    def test_str(self):
         t1 = Task(1,"t1",False)
-        self.assertEqual(t1.is_completed(),False)
+        self.assertEqual(t1.__str__(),"Task 1: t1 (Priority: False, Not Done)")
+
+    def test_is_completed_false(self):
+        t1 = Task(1,"t1", 3, False)
+        isCompleted = t1.is_completed()
+        self.assertEqual(isCompleted,False)
     
-    def test_B(self):
-        t1 = Task(2,"t2")
-        self.assertEqual(t1.__str__(),"Task 2: t2 (Priority: 1, Not Done)")
+    def test_is_completed_true(self):
+        t1 = Task(1,"t1", 1, True)
+        isCompleted = t1.is_completed()
+        self.assertEqual(isCompleted,True)
+
+    def test_complete_task(self):
+        t1 = Task(1,"t1",False)
+        t1.complete_task()
+        self.assertEqual(t1.completed,True)
--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -5,7 +5,70 @@ import unittest
 
 class TestTodoList(unittest.TestCase):
 
-    def test_A(self):
+    def test_list_empty_tasks(self):
         todo = TodoList()
         list = todo.list_tasks()
         self.assertEquals([],list)
+
+    def test_add_task(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        t1 = todo.tasks[0]
+        self.assertEquals(t1.__str__(),"Task 1: t1 (Priority: 1, Not Done)")
+    
+    def test_remove_task_found(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        todo.add_task("t2")
+        result = todo.remove_task(1)
+        self.assertEquals(result,"Task 1 removed.")
+        self.assertEquals(todo.list_tasks(),["Task 2: t2 (Priority: 1, Not Done)"])
+
+    def test_remove_task_not_found(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        todo.add_task("t2")
+        self.assertEquals(todo.remove_task(3),"Task 3 not found.")
+    
+    def test_complete_task_found(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        result = todo.complete_task(1)
+        self.assertEquals(result,"Task 1 marked as done.")
+    
+    def test_complete_task_not_found(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        result = todo.complete_task(3)
+        self.assertEquals(result,"Task 3 not found.")
+
+
+    def test_find_high_priority_tasks_geater(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        todo.add_task("t2")
+        todo.tasks[0].priority = 2
+        todo.tasks[1].priority = 5
+        self.assertEquals(todo.find_high_priority_tasks(3),[todo.tasks[1]])
+
+    def test_find_high_priority_tasks_less(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        todo.add_task("t2")
+        todo.tasks[0].priority = 2
+        todo.tasks[1].priority = 5
+        self.assertEquals(todo.find_high_priority_tasks(6),[])
+
+    def test_find_high_priority_tasks_equal(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        todo.add_task("t2")
+        todo.tasks[0].priority = 2
+        todo.tasks[1].priority = 5
+        self.assertEquals(todo.find_high_priority_tasks(5),[])
+    
+    def test_list_tasks(self):
+        todo = TodoList()
+        todo.add_task("t1")
+        todo.add_task("t2")
+        self.assertEquals(todo.list_tasks(),["Task 1: t1 (Priority: 1, Not Done)","Task 2: t2 (Priority: 1, Not Done)"])
